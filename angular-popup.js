4c9ef8572617cd45562143f64f81be9aedccdb36
Jared Morse <jarcoal@gmail.com>
2015-01-09 11:08:48 -0800

    initial logic

---
+++ angular-popup.js
@@ -0,0 +1,55 @@
+(function() {
+  'use strict';
+
+  angular.module('popup', [])
+
+  /**
+   * @ngdoc service
+   * @name popup.service.PopUp
+   * @module popup
+   * @description
+   * Opens/manages browser pop-up windows.
+   */
+  .service('PopUp', ['$log', '$window', '$q', '$interval',
+    function($log, $window, $q, $interval) {
+      /**
+       * @ngdoc method
+       * @name popup.service.PopUp#open
+       * @description
+       * Opens a new window and returns a promise that will resolve when the
+       * window is closed.
+       *
+       * @param  {string} url to point the window to.
+       * @param  {string} options to apply to the window (likely height/width).
+       */
+      this.open = function(url, options) {
+        $log.debug('PopUp', 'opening', url, options);
+
+        var d = $q.defer();
+
+        // build the options string
+        var optionsString = _.map(options, function(val, key) {
+          return key + '=' + val;
+        }).join(',');
+
+        // open the window
+        var win = $window.open(url, url, optionsString);
+
+        // setup an interval to watch the window for manual dismiss.
+        var watcher = $interval(function() {
+          if (win.closed) {
+            $log.debug('PopUp', 'closed');
+            d.resolve();
+            $interval.cancel(watcher);
+          }
+        }, 100, 0, false);
+
+        // ship back the promise so that the caller can know when the
+        // window closes
+        return d.promise;
+      };
+
+    }
+  ]);
+
+})();
